d5f78812a56760a565db69a006e5a390e9327100
kweckolukasz <kweckolukasz@gmail.com>
2021-05-24 11:06:13 +0200

    Config: Add DB callback with dummy data

--- app/src/main/java/com/kwecko/simpletodolist/MainActivity.java
+++ app/src/main/java/com/kwecko/simpletodolist/MainActivity.java
@@ -1,22 +1,18 @@
 package com.kwecko.simpletodolist;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.kwecko.simpletodolist.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- app/src/main/java/com/kwecko/simpletodolist/database/TodoRoomDatabase.java
+++ app/src/main/java/com/kwecko/simpletodolist/database/TodoRoomDatabase.java
@@ -2,13 +2,17 @@ package com.kwecko.simpletodolist.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.kwecko.simpletodolist.DAO.TodoDAO;
 import com.kwecko.simpletodolist.model.Todo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -29,11 +33,39 @@ public abstract class TodoRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             TodoRoomDatabase.class, "todo_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            databaseWriteExecutor.execute(()->{
+                TodoDAO todoDAO = INSTANCE.todoDAO();
+                todoDAO.deleteAll();
+
+                List<Todo> todos = Arrays.asList(
+                        new Todo(0, "posprzątaj pokój", 123456789),
+                        new Todo(1, "nakarm psa", 12345678),
+                        new Todo(2, "zrób zakupy", 1234567),
+                        new Todo(3, "odbierz oliwkę :)", 123456),
+                        new Todo(4, "Pojedź do Karpacza i wyszlifuj wszystkie balkony", 123456),
+                        new Todo(5, "12345678911234567892345678931234567894123456789512345678961234657897123456789", 123456)
+                );
+
+                for (Todo todo :
+                        todos) {
+                    todoDAO.insertTodo(todo);
+                }
+
+
+            });
+        }
+    };
 
 }
